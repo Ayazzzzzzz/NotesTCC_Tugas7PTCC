49dc461e979151fe339bd7be4a6bd350a7e7ecf1
Capstone_GreenGuard <larasayodya04@gmail.com>
2025-06-03 21:18:10 +0700

    akhhh

--- backend/index.js
+++ backend/index.js
@@ -46,7 +46,7 @@ app.use(cors({
       await sequelize.sync();
   
       const port = process.env.PORT || 5000;
-      app.listen(port, '0.0.0.0', () => console.log(`Server running on port ${port}`));
+      app.listen(port, () => console.log(`Server running on port ${port}`));
     } catch (error) {
       console.error("Unable to connect to the database:", error);
     }
