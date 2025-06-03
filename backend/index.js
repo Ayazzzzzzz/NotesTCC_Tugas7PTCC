ec145548847d681a76ae6975a3ddfd6596f83c14
Capstone_GreenGuard <larasayodya04@gmail.com>
2025-06-03 23:04:29 +0700

    coba lagi

--- backend/index.js
+++ backend/index.js
@@ -6,49 +6,23 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import sequelize from "./config/database.js";
 
-dotenv.config();
-
 const app = express();
-
 app.set("view engine", "ejs");
-
-const allowedOrigins = ['https://dhea-notes-dot-e-13-450704.uc.r.appspot.com']; 
-
-console.log("Allowed origins:", allowedOrigins);
 
 app.use(cookieParser());
 
-app.use(cors({
-    origin: function(origin, callback) {
-      if(!origin) return callback(null, true);
-      if(allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true
-  }));
-  
-  app.use(express.json());
-  
-  app.get("/", (req, res) => res.render("index"));
-  
-  app.use(UserRoute);
-  app.use(NotesRoutes);
+app.use(
+  cors({
+    origin: "https://notesgita-dot-g-07-450802.uc.r.appspot.com",
+    credentials: true,
+  })
+);
 
-  const start = async () => {
-    try {
-      await sequelize.authenticate();
-      console.log("Database connected");
-      await sequelize.sync();
-  
-      const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.use(express.json());
 
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-  };
-  
-  start();
+app.get("/", (req, res) => res.render("index"));
+
+app.use(UserRoute);
+app.use(NotesRoutes);
+
+app.listen(5000, () => console.log("Server connected"));
